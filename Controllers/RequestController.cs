7fba109980d537421b7365b68f87d3951839c98c
vanshika0508 <vermvans582000@gmail.com>
2025-09-22 13:44:47 -0400

    final changes

--- Controllers/RequestController.cs
+++ Controllers/RequestController.cs
@@ -1,38 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using EquipmentApp.Models;
 
-/*namespace EquipmentApp.Controllers
-{
-    public class RequestController : Controller
-    {
-        // GET request from user
-        public IActionResult RequestForm()
-        {
-            return View();
-        }
-
-        // POST the request
-        [HttpPost]
-        public IActionResult RequestForm(EquipmentRequest request)
-        {
-            if (ModelState.IsValid && request.Duration > 0)
-            {
-                RequestRepository.Add(request);
-                return RedirectToAction("Confirmation");
-            }
-
-            ViewBag.Error = "Please check your inputs.";
-            return View(request);
-        }
-
-        public IActionResult Confirmation()
-        {
-            return View();
-        }
-    }
-}*/
-//using Microsoft.AspNetCore.Mvc;
-//using EquipmentApp.Models;
 
 namespace EquipmentApp.Controllers
 {
--- obj/Debug/net9.0/ToolsBorrow.AssemblyInfo.cs
+++ obj/Debug/net9.0/ToolsBorrow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToolsBorrow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a3deba21303f0d1844d07263496d035cbba98c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20667437b5c21017515580df9bfbb7ab29fe1b2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToolsBorrow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToolsBorrow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
